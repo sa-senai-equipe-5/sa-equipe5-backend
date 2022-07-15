8ae73cc35ecbbcccc5a9179f9d79dfbdb7592e7f
Ulisses Viana Bardini <ulisses_bardini@estudante.sc.senai.br>
2022-07-15 19:41:54 -0300

    atualização de anotações

--- src/main/java/br/com/senai/saequipe5backend/entity/Entrega.java
+++ src/main/java/br/com/senai/saequipe5backend/entity/Entrega.java
@@ -8,10 +8,9 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
-import javax.validation.constraints.Max;
-import javax.validation.constraints.Min;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 import br.com.senai.saequipe5backend.enums.Entregue;
 import lombok.Data;
@@ -28,22 +27,25 @@ public class Entrega {
 	@Column(name = "id")
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
+	
 	@NotEmpty(message = "O endereço é obrigatório!")
 	@Column(name = "endereco")
-	@Min(value = 2, message = "O endereço deve possuir no mínimo 2 caracteres")
-	@Max(value = 500, message = "O endereço deve possuir no máximo 500 caracteres")
+	@Size(min = 2, max = 500, message = "O endereço deve possuir entre 2 e 500 caracteres")
 	private String enderecoCompleto;
+	
 	@NotNull(message = "A data de entrega é obrigatória!")
 	@Column(name = "dt_entrega")
 	private LocalDate dataDeEntrega;
+	
 	@NotNull(message = "O campo Entregue é obrigatório")
 	@Column(name = "entregue")
 	private Entregue entregue;
+	
 	@NotEmpty(message = "A descrição é obrigatória!")
 	@Column(name = "descricao")
-	@Min(value = 10, message = "A descrição deve possuir no mínimo 10 caracteres")
-	@Max(value = 1500, message = "A descrição deve possuir no máximo 1500 caracteres")
+	@Size(min = 10, max = 1500, message = "A descrição deve possuir entre 10 e 1500 caracteres")
 	private String descricao;
+	
 	@NotNull(message = "O entregador é obrigatório")
 	@Column(name = "id_entregador")
 	private Entregador entregador;
--- src/main/java/br/com/senai/saequipe5backend/entity/Usuario.java
+++ src/main/java/br/com/senai/saequipe5backend/entity/Usuario.java
@@ -6,12 +6,11 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
-import javax.validation.constraints.Max;
-import javax.validation.constraints.Min;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
 
 import br.com.senai.saequipe5backend.enums.Perfil;
 import lombok.Data;
@@ -28,23 +27,24 @@ public class Usuario {
 	@EqualsAndHashCode.Include
 	@Column(name = "id")
 	private Integer id;
+	
 	@Column(name = "nm_completo")
 	@NotEmpty(message = "O nome é obrigatório!")
-	@Min(value = 2, message = "O nome deve possuir no mínimo 2 caracteres")
-	@Max(value = 50, message = "O nome deve possuir no máximo 50 caracteres")
+	@Size(min = 2, max = 50, message = "O nome deve possuir entre 2 e 50 caracteres")
 	private String nomeCompleto;
+	
 	@Column(name = "login")
 	@NotEmpty(message = "O login não pode ser nulo!")
-	@Min(value = 2, message = "O login deve possuir no mínimo 2 caracteres")
-	@Max(value = 20, message = "O login deve possuir no máximo 20 caracteres")
+	@Size(min = 2, max = 20, message = "O login deve possuir entre 2 e 20 caracteres")
 	private String login;
+	
 	@Column(name = "senha")
 	@NotEmpty(message = "A senha não pode ser nula!")
 	@NotBlank(message = "A senha não pode conter espaços!")
 	@Pattern(regexp = "^[A-Za-z0-9_-]*$", message = "A senha deve conter letras e números")
-	@Min(value = 2, message = "A senha deve possuir no mínimo 2 caracteres")
-	@Max(value = 10, message = "A senha deve possuir no máximo 10 caracteres")
+	@Size(min = 2, max = 10, message = "A senha deve possuir entre 2 e 10 caracteres")
 	private String senha;
+	
 	@NotNull(message = "O perfil não pode ser nulo!")
 	@Column(name = "perfil")
 	private Perfil perfil;
